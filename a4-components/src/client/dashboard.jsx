4c75af323d288b64aa5c93b923bc5b804894a8ef
jeremymkurtz <kurtzjeremy126@gmail.com>
2024-09-26 03:30:26 -0400

    revert

---
+++ a4-components/src/client/dashboard.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+import editModal, {editMatch} from "./components/editModal.jsx";
+import form, {add} from "./components/form.jsx";
+import {generateMatches} from "./components/matches.jsx";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+window.onload = async function() {
+    await generateMatches()
+
+
+    document.querySelector('.modal-close').addEventListener('click', () => {
+        document.getElementById('editModal').style.display = 'none';
+    });
+    document.querySelector('.cancel-modal').addEventListener('click', () => {
+        document.getElementById('editModal').style.display = 'none';
+    });
+
+
+    // window.onclick = function(event) {
+    //     if (event.target === document.getElementById('editModal')) {
+    //         document.getElementById('editModal').style.display = 'none';
+    //     }
+    // };
+
+
+}
+async function onLogout(event, navigate) {
+
+    const response = await fetch('/logout', {
+        method: 'GET',
+    });
+    if (response.ok) {
+        navigate('/'); // Redirect to the root URL
+    } else {
+        console.error('Logout failed');
+    }
+}
+
+function Dashboard() {
+    const navigate = useNavigate()
+    return (
+        <>
+            <h1 className="title is-family-primary is-size-1 pt-6 is-flex is-flex-direction-row">Submit your Match! <button
+                id="logoutButton" className="button is-info" onClick={(e) => onLogout(e, navigate)}>Logout</button>
+            </h1>
+            <div>
+
+                {form()}
+                <div id="matches-container">
+
+                </div>
+                {editModal()}
+
+            </div>
+        </>
+    );
+}
+
+export default Dashboard;
