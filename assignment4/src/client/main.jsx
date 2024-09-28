23c39f9c4af99171580fffe951e8cf9c5203060d
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2024-09-28 16:03:54 -0400

    mostly completed assignment 4

---
+++ assignment4/src/client/main.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+//in template code this is in index.jsx
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
