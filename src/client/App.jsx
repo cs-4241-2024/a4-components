b19368d7aa638b7f3f2830551bb741d37cc30a17
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-09-29 03:45:35 -0400

    Before I break something more

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,34 +1,189 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+let loggedIn = false;
+let lastRow = {};
+let username = null;
+let password = null;
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+class Todo extends React.Component {
+	// our .render() method creates a block of HTML using the .jsx format
+	render() {
+		if (loggedIn) {
+			return (
+				<tr>
+					<td>${this.props.classCode}</td>
+					<td>${this.props.className}</td>
+					<td>${this.props.assignment}</td>
+					<td>${this.props.daysLeft}</td>
+					<td>${this.props.dueDate}</td>
+				</tr>
+			)
+		}
+		else {
+			return;
+		}
+	}
+}
+
+// main component
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		// initialize our state
+		this.state = { todos: [] }
+		this.load()
+
+	}
+
+	//Getting data for the table
+	getData() {
+		if (loggedIn) {
+			fetch('/data/${username}', {
+				method: 'GET',
+				headers: { "Content-Type": "application/json" }
+			})
+				.then(response => response.json())
+				.then(json => {
+					this.setState({ todos: json })
+				})
+
+			// showData(json);
+		}
+	}
+
+	//TODO: Submit function here
+
+	deleteRow() {
+		if (loggedIn && lastRow != {}) {
+			fetch('/delete', {
+				method: 'POST',
+				body: JSON.stringify(lastRow),
+				headers: { "Content-Type": "application/json" },
+			})
+				.then(response => response.json())
+				.then(json => {
+					// getData(json)
+					// getData()
+					//Doesn't work so copy and pasting code
+					if (loggedIn) {
+						fetch('/data/${username}', {
+							method: 'GET',
+							headers: { "Content-Type": "application/json" }
+						})
+							.then(response => response.json())
+							.then(json => {
+								this.setState({ todos: json })
+							})
+
+						// showData(json);
+					}
+				})
+		}
+	}
+
+	logIn() {
+		console.log("gotcalled")
+		if (!loggedIn) {
+			username = document.querySelector('#Username').value;
+			password = document.querySelector('#Password').value;
+
+			const newData = {
+				"Username": username,
+				"Password": password
+			};
+			fetch('/logIn', {
+				method: 'POST',
+				body: JSON.stringify(newData),
+				headers: { "Content-Type": "application/json" }
+			})
+				.then(response => response.json())
+				.then(json => {
+					console.log(json);
+					console.log("Username " + json.Username);
+					console.log("Password " + json.Password);
+					if (json.Username != username || json.Password != password) {
+						//Making the call again incase during the last call a new user was made
+						fetch('/logIn', {
+							method: 'POST',
+							body: JSON.stringify(newData),
+							headers: { "Content-Type": "application/json" }
+						})
+							.then(response2 => response2.json())
+							.then(json2 => {
+								if (json2.Username != username || json2.Password != password) {
+									alert("Your password is incorrect!");
+								}
+								else {
+									loggedIn = true;
+									//Doesn't work so copy and pasting code
+									if (loggedIn) {
+										fetch('/data/${username}', {
+											method: 'GET',
+											headers: { "Content-Type": "application/json" }
+										})
+											.then(response => response.json())
+											.then(json => {
+												this.setState({ todos: json })
+											})
+
+										// showData(json);
+									}
+								}
+							})
+					}
+					else {
+						loggedIn = true;
+						//Doesn't work so copy and pasting code
+						if (loggedIn) {
+							fetch('/data/${username}', {
+								method: 'GET',
+								headers: { "Content-Type": "application/json" }
+							})
+								.then(response => response.json())
+								.then(json => {
+									this.setState({ todos: json })
+								})
+
+							// showData(json);
+						}
+					}
+				})
+		}
+	}
+
+
+	// load in our data from the server
+	load() {
+		fetch('/read', { method: 'get', 'no-cors': true })
+			.then(response => response.json())
+			.then(json => {
+				this.setState({ todos: json })
+			})
+	}
+
+	// render component HTML using JSX 
+	//I think will call the render of each element
+	render() {
+		return (
+			<div className="App">
+				<form id="logInForm">
+					<input type="text" id="Username" placeholder="Username" />
+					<input type="text" id="Password" placeholder="Password" />
+					<button className="button" type="button" id="logInButton" onClick={this.logIn}>Log In</button>
+				</form>
+				<form id="homeworkList">
+					<input type="text" id="Code" placeholder="Class Code" />
+					<input type="text" id="Name" placeholder="Class Name" />
+					<input type="text" id="Assignment" placeholder="Assignment" />
+					<input type="text" id="Days" placeholder="Days Left" />
+					<button className="button" type="button" id="submitButton" onClick={this.submit}>Submit</button>
+					<button className="button" type="button" id="deleteButton" onClick={this.deleteRow}>Delete Last Row</button>
+				</form>
+				{this.state.todos.map((todo, i) => <Todo username={todo.username} password={todo.password} />)}
+
+			</div>
+		)
+	}
 }
 
 export default App;
--- src/server/main.js
+++ src/server/main.js
@@ -1,26 +1,71 @@
 import express from 'express'
 import ViteExpress from 'vite-express'
+import { MongoClient, ObjectId } from 'mongodb'
+import { } from 'dotenv/config'
 
-const app = express()
-
-const todos = [
-  { name: 'buy groceries', completed: false }
-]
-
+// const { MongoClient, ObjectId } = require("mongodb");
+const app = express();
+// require("dotenv").config();
 app.use(express.json())
+const uri = `mongodb+srv://supermanbritt2003:${process.env.db_password}@a3.cfniv.mongodb.net/?retryWrites=true&w=majority&appName=A3`
+const client = new MongoClient(uri)
 
-app.get('/read', (req, res) => res.json(todos))
+let collection = null
 
-app.post('/add', (req, res) => {
-  todos.push(req.body)
-  res.json(todos)
+async function run() {
+  await client.connect()
+  collection = await client.db("A3").collection("test");
+}
+run();
+
+//Get to-do list data
+app.get("/data/:username", async (req, res) => {
+  if (collection !== null) {
+    const username = req.params.username;
+    const docs = await collection.find({ Username: username }).toArray();
+    res.end(JSON.stringify(docs))
+  }
 })
 
-app.post('/change', function (req, res) {
-  const idx = todos.findIndex(v => v.name === req.body.name)
-  todos[idx].completed = req.body.completed
+//Submit new to-do list item
+app.post('/submit', async (req, res) => {
+  const result = await collection.insertOne(req.body);
+  // res.json(result);
+  res.end(JSON.stringify(result));
+})
 
-  res.sendStatus(200)
+app.post('/logIn', async (req, res) => {
+  const username = req.body.Username;
+  const password = req.body.Password;
+  const searchResults = await collection.find({ Username: username }).toArray();//might need to be a string
+  if (searchResults.length > 0) {
+    console.log("Username exists")
+    //There is a matching username
+    const tempUsername = searchResults[0].Username;
+    const tempPassword = searchResults[0].Password;
+    console.log("tempUsername " + tempUsername);
+    console.log("tempPassword " + tempPassword);
+    console.log(JSON.stringify(searchResults));
+    if (tempPassword == password) {
+      //Password matches
+      res.end(JSON.stringify(searchResults[0]))
+    }
+    else {
+      //Password is wrong
+      res.end(JSON.stringify(searchResults[0]))
+    }
+  }
+  else {
+    //Insert new username and password
+    const result = await collection.insertOne(req.body);
+    res.end(JSON.stringify(result))
+  }
+})
+
+app.post('/delete', async (req, res) => {
+  const result = await collection.deleteOne(req.body);//need to get the data for the last item
+  // res.json(result);
+  res.end(JSON.stringify(result));
 })
 
 ViteExpress.listen(app, 3000)
