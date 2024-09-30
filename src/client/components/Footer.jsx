13153dca2405e59ef8a4726ab0fafad92df53e88
arcash4 <arcash@wpi.edu>
2024-09-30 00:41:37 -0400

    commit

---
+++ src/client/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Created by Andrew Cash</p>
+      <p>Email: arcash@wpi.edu</p>
+    </footer>
+  );
+};
+
+export default Footer;
