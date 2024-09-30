8e04067072efa4e0ea9a26ef592195fda2824c6a
sagurgol <sagurgol@wpi.edu>
2024-09-29 21:43:59 -0400

    idk

--- a4-sagurgol/src/client/coolForm.jsx
+++ a4-sagurgol/src/client/coolForm.jsx
@@ -19,8 +19,8 @@ const coolForm = ({label1, label2, label3, label4, label5}) => {
         <label for="Op5">{label5}</label>
         <button onClick={setCools((cools) => cools + document.querySelectorAll('input[type="checkbox"]:checked').length)}>submit</button>
       </form>
-        <p class= "Results" id="Result1"><strong id="Reg"> You are:</strong></p>
-        <p class= "Results" id="Result2"><strong id="Super"> {cools*20}% Cool!</strong></p>
+        <p className= "Results" id="Result1"><strong id="Reg"> You are:</strong></p>
+        <p className= "Results" id="Result2"><strong id="Super"> {cools*20}% Cool!</strong></p>
       </div>
     )
   }
