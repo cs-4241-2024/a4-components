d65738e3e5a0ecc1597419ae7867a547bfa685fd
Vidushi Meel <vidushimeel@Vidushis-MBP.lan>
2024-10-12 19:01:12 -0400

    create: assignment 4

---
+++ src/client/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import VehicleForm from './frontend/VehicleForm';
+
+function App() {
+  return (
+    <div className="App">
+      <VehicleForm />
+    </div>
+  );
+}
+
+export default App;
